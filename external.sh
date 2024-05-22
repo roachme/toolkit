c97810cb4b86008cd5ef621d957c4bf19bfc3a71
roach <qcockroach6@gmail.com>
2024-05-22 08:11:03 +0300

    external.sh: Fix nvim installation function

--- external.sh
+++ external.sh
@@ -5,13 +5,11 @@ function prepare()
 
 function external_nvim()
 {
-    git clone --depth 1 git@github.com:neovim/neovim.git && cd nvim
+    git clone --depth 1 https://github.com/neovim/neovim.git && cd nvim
     git checkout stable
-    make CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX=~/.local/bin
+    make CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX=~/.local
     make install
 
     # clean up
     cd .. && rm -rf nvim
 }
-
-
