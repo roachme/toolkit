92e7bc1a6dc4904531da5f753ca2e6005e6a7c02
roach <qcockroach6@gmail.com>
2024-05-25 10:32:04 +0300

    extern: nvim: Add nvim packer

--- extern/nvim.sh
+++ extern/nvim.sh
@@ -2,6 +2,9 @@ function external_nvim()
 {
     local nvim_dir="neovim"
     local nvim_url="https://github.com/neovim/neovim.git"
+    local packer_dir="~/.local/share/nvim/site/pack/packer/start/packer.nvim"
+    local packer_url="https://github.com/wbthomason/packer.nvim"
+
 
     git clone --branch=stable --depth 1 "$nvim_url" "$nvim_dir"
 
@@ -11,6 +14,7 @@ function external_nvim()
     cd ..
 
     rm -rf "$nvim_dir" # delete source code
+    git clone --depth 1 "$packer_url" "$packer_dir"
 }
 
 external_nvim
